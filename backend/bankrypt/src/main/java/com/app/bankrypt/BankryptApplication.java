8aaef4c5f7e5af16bd3708e2f2dc2f62c4eef07e
ryantz <ryantanzhi1997@gmail.com>
2025-05-15 20:44:26 +0800

    first commit

---
+++ backend/bankrypt/src/main/java/com/app/bankrypt/BankryptApplication.java
@@ -0,0 +1,12 @@
+package com.app.bankrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankryptApplication.class, args);
+	}
+}
