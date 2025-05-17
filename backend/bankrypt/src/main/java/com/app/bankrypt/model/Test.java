faa5a5bb610d14e2c6aa95a35d7b327f2ee3c642
ryantz <ryantanzhi1997@gmail.com>
2025-05-17 08:08:46 +0800

    test

---
+++ backend/bankrypt/src/main/java/com/app/bankrypt/model/Test.java
@@ -0,0 +1,21 @@
+package com.app.bankrypt.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="testdb")
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="string_test", nullable=true)
+    private String value;
+}
---
+++ backend/bankrypt/src/main/java/com/app/bankrypt/repository/TestRepository.java
@@ -0,0 +1,9 @@
+package com.app.bankrypt.repository;
+
+import com.app.bankrypt.model.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
+}
