5d500e6cc21df6e946dd1693532ace8051c328ff
ryantz <ryantanzhi1997@gmail.com>
2025-05-26 08:32:25 +0800

    fe

--- frontend/bankrypt-fe/src/app/app.component.ts
+++ frontend/bankrypt-fe/src/app/app.component.ts
@@ -9,26 +9,30 @@ import { Component } from '@angular/core';
 */
 
 @Component({
-  selector: 'app-root',
-  template: `Hello, the {{title}} is under construction`,
+  selector: 'app-root', // name to ref to in other templates
+  template: `
+  @if (isServerRunning){
+    Hello server is running
+  }
+  @else {
+   the app {{title}} is under construction, server is not running 
+  }
+  `,
   styles: `
   :host {
     color: black
   }`,
-
 })
 export class AppComponent {
+  // component properties
   title = 'bankrypt-fe';
+  isServerRunning = true;
 }
 
-/*
 @Component ({
-  selector: 
-
+  selector: 'app-user',
+  template: `<section><app-user /></section>`,
+  imports: [UserComponent],
 })
-  */
 export class UserComponent {
-
 }
-
-
